55d05abb03a0ed7c6d7127d045645ffd7aa0a15d
Zach Weiss <zach@zachweiss.com>
2016-05-11 11:12:38 -0400

    ok i think i got it

--- trackRGB.cpp
+++ trackRGB.cpp
@@ -115,7 +115,7 @@ int main(int argc, char** argv)
  			std::cout << "Beta: " << beta << std::endl;
 
 		}
-        
+        //ok
 		imgOriginal = imgOriginal + imgLines;
 		Mat outOriginal = imgOriginal;
 		flip(outOriginal, outOriginal, 1);
